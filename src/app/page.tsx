966b97225d239574cc426e41e98b1adae60ae02a
Patrycja Petryk <patrycja.petryk@gmail.com>
2023-09-07 17:31:50 +0200

    Add homepage page type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
-export default function Home() {
-  return (
-    <main>
-      <h1 className='font-display text-xl'>Hello World!</h1>
-    </main>
-  );
+import { Metadata } from 'next';
+import { SliceZone } from '@prismicio/react';
+
+import { createClient } from '@/prismicio';
+// import { components } from '@/slices';
+
+export default async function Home() {
+  const client = createClient();
+  const page = await client.getSingle('homepage');
+
+  return <h1 className='font-display text-xl'>{page.data.title}</h1>;
+  // return <SliceZone slices={page.data.slices} components={components} />;
+}
+
+export async function generateMetadata(): Promise<Metadata> {
+  const client = createClient();
+  const page = await client.getSingle('homepage');
+
+  return {
+    title: page.data.meta_title,
+    description: page.data.meta_description,
+  };
 }
