79ef8c68f62d2a2725fa9442eda1846293f19ef2
Patrycja Petryk <patrycja.petryk@gmail.com>
2023-09-07 12:18:24 +0200

    Add fallback metadata to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Red_Hat_Mono, Chewy } from 'next/font/google';
+import { createClient } from '@/prismicio';
 
 export const redHatMono = Red_Hat_Mono({
   subsets: ['latin-ext'],
@@ -15,6 +16,22 @@ export const chewy = Chewy({
   variable: '--font-chewy',
   display: 'swap',
 });
+
+export async function generateMetadata(): Promise<Metadata> {
+  const client = createClient();
+
+  const settings = await client.getSingle('settings');
+
+  return {
+    title: settings.data.site_title || 'Leszczewo 15',
+    description:
+      settings.data.site_description ||
+      'Oferujemy do wynajęcia dom na skraju lasu, na terenie Wigierskiego Parku Narodowego.',
+    openGraph: {
+      images: [settings.data.og_image.url || ''],
+    },
+  };
+}
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
